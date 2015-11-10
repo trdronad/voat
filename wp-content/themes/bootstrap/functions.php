de946e54544d160c20aabf79ff895963e9d94025
jam <jam@192.168.1.40>
2015-11-10 15:04:21 +0800

    fixed artist ranking and initial design for all posts

--- wp-content/themes/bootstrap/functions.php
+++ wp-content/themes/bootstrap/functions.php
@@ -33,6 +33,31 @@ $defaults = array(
 		'admin-preview-callback' => '',
 );
 add_theme_support( 'custom-header', $defaults );
+//display post in ranking
+function getPostViews($postID){
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 View";
+    }
+    return $count.' Views';
+}
+function setPostViews($postID) {
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+// Remove issues with prefetching adding extra views
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
 //Add content width (desktop default)
 if ( ! isset( $content_width ) ) {
--- wp-content/themes/bootstrap/single.php
+++ wp-content/themes/bootstrap/single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	<?php
-	while ( have_posts() ) : the_post();
-		get_template_part(  the_content(), get_post_format() );
+	while ( have_posts() ) : the_post();        		setPostViews(get_the_ID());
+		get_template_part(  the_content(), get_post_format() );			
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
 		endif;
