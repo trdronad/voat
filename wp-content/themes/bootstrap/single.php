646eda02ba1032af360766af80d9f370519e6a0c
trd-khent <trd.khent@gmail.com>
2015-11-10 13:22:54 +0800

    news2

--- wp-content/themes/bootstrap/single.php
+++ wp-content/themes/bootstrap/single.php
@@ -1,12 +1,37 @@
 <?php get_header(); ?>
+
+	
+<?php
+
+	while ( have_posts() ) : the_post(); ?>
+
+		<?php if ( in_category('news') ) { ?>
+		<h3 class="news-date">News | <?php echo the_time('Y.m.d'); ?></h3>
+		<br />
+		<h3 class="category-news"><?php echo the_title(''); ?></h3>
+		<?php } else { ?>
+		<p></p>
+		<?php } ?>
+		<br />
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+
 	<?php
-	while ( have_posts() ) : the_post();
+
 		get_template_part(  the_content(), get_post_format() );
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
-		endif;
-	endwhile;
+		endif; 
 	?>
+	
+	</article>
+
+	<?php
+
+	endwhile;
+
+?>
+
+
 <?php get_footer(); ?>
 
 
