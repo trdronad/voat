c8ba39aa03e3c479229f518a8f79d1f3d8ddef55
trdjason <trd.jason@gmail.com>
2015-11-12 14:59:48 +0800

    designing artist 2

--- wp-content/themes/bootstrap/single.php
+++ wp-content/themes/bootstrap/single.php
@@ -1,12 +1,14 @@
+<?php /* Template Name: Artist2 Template */ ?>
 <?php get_header(); ?>
 	<div class="artist_container">
 		<?php
-		if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="artist_post">
-			<?php the_content(); ?>
+		if (have_posts()) : the_post(); ?>
+		<div class="artist_post"> 
+			<?php the_post_thumbnail();?>
 			<h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
 		</div>
-		<?php endwhile; endif; ?>
+		<?php the_meta();  ?>
+		<?php  endif; ?>
 	</div>
 <div class="container-fluid artist2 min-h">
 	<div class="row">
@@ -16,22 +18,14 @@
 			</div>
 			<div class="row">
 				<div class="col-lg-3 side">
-					<h1>PROFILE</h1>
+					<h1>PROFILE</h1> 
 						<div class="side-box">
 							<?php
-								if (have_posts()) : while (have_posts()) : the_post(); ?>
+								if (have_posts()) : the_post(); ?>
 									<div class="artist_profile">
-									<?php the_content(); ?>
+									<?php echo get_post_meta(get_the_ID(), 'Music', true); ?>
 									</div>
-							<?php endwhile; endif; ?>
-							<h5>Name: Your name here</h5>
-							<h5>Gender: Your gender here</h5>
-							<h5>Nationality: Your nationality here</h5>
-							<h5>Religion: Your religion here</h5>
-							<h5>Age: Your age here</h5>
-							<h5>Address: Your address here</h5>
-							<h5>Likes: Your likes here</h5>
-							<h5>Dislikes: Your dislikes here</h5>
+							<?php endif; ?>
 						</div>
 							<ul class="soc-name">
 								<li><a href="https://twitter.com" target="_blank">Twitter</a></li>
@@ -41,9 +35,9 @@
 							</ul>
 						<div class="contactbox">
 							<h1 class="text-center">CONTACT</h1>
-							<h3 class="text-center">サンプルサンプル</h3>
+							<h3 class="text-center">このアーティストに興味を持っています</h3>
 						</div>
-							<h4 class="text-right">サンプルサンプル</h4>
+							<h4 class="text-right">スタッフから返信されます。</h4>
 				</div>
 				<div class="col-lg-9 main">
 					<h1 style="margin-bottom: 20px;">MUSIC</h1>
@@ -52,8 +46,8 @@
 								<h1>サンプル</h1>
 							</div>
 							<div class="col-lg-9 musicrec">
-								<h1 class="s1">Sound Cloud</h1>
-								<h1>Sound Cloud</h1>
+								<?php echo do_shortcode('[soundcloud]http://soundcloud.com/forss/flickermood[/soundcloud]'); ?><br>
+								<?php echo do_shortcode('[soundcloud]http://soundcloud.com/forss/flickermood[/soundcloud]'); ?>
 							</div>
 						</div>
 							<h3>OOOOOOOSingle</h3>
@@ -68,7 +62,12 @@
 						</div>
 							<h1>NEWS</h1>
 						<div class="artist2-news">2015.10.20<strong>12/24OOサンプルサンプルサンプル</strong></div>
-						<h1 style="margin-top: 50px;">COMMENTS</h1>
+						<h1 style="margin-top: 50px;">COMMENT</h1>
+						<article id="post-<?php the_ID(); ?>">
+						<?php 
+						if ( comments_open() || get_comments_number() ) : comments_template(); 
+						endif; ?>
+						</article>
 				</div>
 		</div>
 	</div>
