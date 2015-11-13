faba72a9a75819697a36286cd0a3ea86f7283d6d
trd-jonathan <trd.jonathan@gmail.com>
2015-11-13 14:51:26 +0800

    Artist post template [unfinish]

--- wp-content/themes/bootstrap/functions.php
+++ wp-content/themes/bootstrap/functions.php
@@ -181,3 +181,44 @@ if ( ! function_exists( 'bootstrap_setup' ) ):
  	}
 endif;
 ?>
+
+<?php
+///==================Jonathan Add ==============================
+/**
+* Define a constant path to our single template folder*/
+define(SINGLE_PATH, TEMPLATEPATH . '/single');
+
+/**
+* Filter the single_template with our custom function*/
+add_filter('single_template', 'my_single_template');
+
+/**
+* Single template function which will choose our template*/
+function my_single_template($single) {
+	global $wp_query, $post;
+
+/*** Checks for single template by ID*/
+	if(file_exists(SINGLE_PATH . '/single-' . $post->ID . '.php'))
+		return SINGLE_PATH . '/single-' . $post->ID . '.php';
+	/**
+	* Checks for single template by category
+	* Check by category slug and ID*/
+	foreach((array)get_the_category() as $cat) :
+
+		if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
+			return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
+
+		elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
+			return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
+
+	endforeach;
+/*** Checks for default single post files within the single folder*/
+	if(file_exists(SINGLE_PATH . '/single.php'))
+		return SINGLE_PATH . '/single.php';
+
+	elseif(file_exists(SINGLE_PATH . '/default.php'))
+		return SINGLE_PATH . '/default.php';
+return $single;
+
+}
+?>
